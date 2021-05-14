d0adb1ea9c01d982aa90c666464b9b1e6c4f568b
antony <tonyjaenpop@gmail.com>
2021-05-13 19:32:52 -0600

    134

--- Components/cardsP/cardsP.js
+++ Components/cardsP/cardsP.js
@@ -7,7 +7,7 @@ const CardsP = () => {
   const data = [
     { title: "Hotel-Angular", descS: "Una aplicación web diseñada para la gestión de un hotel creada con el fin de aprendizaje ", descE: "Is a web application developed to the management of the hotel for educational purposes.", imgURL: "/Hotel-Angular.png",video:"/primera.mp4" },
     { title: "Venta", descS: "Un sitio web dinámico creado para administración de ventas de una pequeña empresa", descE: "dynamic Website made to manage the sales of a startup business for educational purposes.", imgURL: "/Venta.png",video:"/Ventas.mp4" },
-    { title: "Venta-Ionic", descS: "Adaptación de una aplicación web a aplicación móvil con tecnologías diferentes", descE: "using new and innovative technologies i created an a mobile application base on a website", imgURL: "/Venta-ionic.png",video:"/celular1.mp4" }
+    { title: "Venta-Ionic", descS: "Adaptación de una aplicación web a aplicación móvil con tecnologías diferentes", descE: "using new and innovative technologies i created an a mobile application base on a website", imgURL: "/Venta-Ionic.png",video:"/celular1.mp4" }
   ]
   const MyContext = useContext(Context)
   const [show, setShow] = useState(false)
@@ -32,7 +32,7 @@ const CardsP = () => {
               data.map(i => (
                 <div className="col-md-4 pt-3 pb-3">
                   <CardP title={i.title} desc={i.descE} imgURL={i.imgURL}>
-                    <Button onClick={()=> {showV(true,i.video)}} className="mt-2" variant="success">Conocer mas</Button>
+                    <Button onClick={()=> {showV(true,i.video)}} className="mt-2" variant="success">More about it</Button>
                   </CardP>
                 </div>
               ))
