efd48ed06413628f3eaf5892ab839b5ffe424019
antony <tonyjaenpop@gmail.com>
2021-05-15 05:27:14 -0600

    resposive changes 5

--- Components/Skills/skill.js
+++ Components/Skills/skill.js
@@ -2,13 +2,20 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 const Skill = ({name,imgURL})=>{
     return (
-        <div className={"skill col-3-md col-3-lg col-3-xl col-3-xll col-3-sm mx-4 my-3 skills"}>
-            <div class="tooltip-inner">
+        <div className={"skill mx-4 my-3 skills"}>
+            <OverlayTrigger
+            placement={"top"}
+            overlay={
+                <Tooltip id={`tooltip-top`}>
                 {name}
-            </div>
+                </Tooltip>
+            }
+            >
              <img src={imgURL}/>
+            </OverlayTrigger>
+          
         </div>
-    ) 
+    )
   }
 
 export default Skill
