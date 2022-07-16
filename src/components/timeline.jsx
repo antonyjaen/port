85e4518df87e6a1e9530bfdbc0bfbdf0ade6ee0d
tony jaen viquez <antony.jaen@kaptyn.com>
2022-07-15 22:34:53 -0600

    timeline push siuuuu *puede morir*

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,13 +1,77 @@
 import React from 'react'
-import { motion } from "framer-motion";
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import { Chrono } from "react-chrono";
 
-export const Timeline = () => {
-  return (
-    <>
+
+export const Timeline = ({color}) => {
+  const items = [
+    {
+      title: "2022/2 - Currently",
+    },
+    {
+      title: "7 Months",
+    },
+    {
+      title: "2016 - 2021"
+    }
+  ];
+  return ( <div style={{ width: "90%", height: "1200px",marginLeft:"5%" }}>
+  <Chrono items={items} mode="VERTICAL_ALTERNATING" 
+  
+    buttonTexts={{
+      first: null,
+      last:  null,
+      next:  null,
+      previous:  null,
+    }}
+
+    fontSizes={{
+      cardSubtitle: '2rem',
+      cardText: '0.8rem',
+      cardTitle: '1rem',
+      title: '1rem',
+    }}
     
-    </>
+    theme={{
+      primary: color,
+      secondary:color,
+      cardBgColor: '#2d2d2d50',
+      cardForeColor: "white",
+      titleColor: 'white',
+      titleColorActive: 'white',
+    } 
+    }>
+        <div>
+        <h3 style={{color : (color == '#1c3a4e' ? '#336484': color ), fontSize:"2.2em"}}>Nearlinx - QA Automation </h3>
+        <p style={{color: 'white', fontSize:"1.1em"}} >
+
+            Create an automated solution to QA process with tools like Cypress, browserstack, selenium and appium
+        </p>
+      </div>
+
+      <div>
+        <h3 style={{color : (color == '#1c3a4e' ? '#336484': color ), fontSize:"2.2em"}}>Nearlinx - Internship</h3>
+        <p style={{color: 'white', fontSize:"1.1em"}} >
+          <span style={{color: 'white', fontSize:"1.3em"}}> Creating a project from
+        scratch with new technologies      </span>
+            <br/>
+        
+                 ... React, Express, Prisma ORM,
+              PostgreSQL,Git,Github and
+              AWS EC2
+        </p>
+      </div>
+
+      <div>
+        <h3 style={{color : (color == '#1c3a4e' ? '#336484': color ), fontSize:"2.2em"}}>Middle Technician In Software Development</h3>
+        <p style={{color: 'white', fontSize:"1.1em"}} >
+         
+          Colegio Técnico Profesional Mercedes Norte.  Certificate in Junior Achievement Costa Rica and IBM - Start Up Challenge
+
+          
+        </p>
+      </div>
+  </Chrono>
+</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,15 @@
 import { useState, useEffect } from 'react'
-import { useParallax } from 'react-scroll-parallax'
-import { ParallaxProvider } from "react-scroll-parallax"
-import ParticlesBg from 'particles-bg'
-import Lottie from "lottie-react";
-import { useLottie } from "lottie-react";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 
-/* react,angular,ionic,mongo,prisma,pg,python,ts,solidity,git,node*/
-
-import reactJson from "../imgs/296-react-logo.json"
-import angularJson from "../imgs/animation_l4b3ehch.json"
-import ionicJson from "../imgs/ionic.json"
-import mongoJson from "../imgs/mongo.json"
-import prismaJson from "../imgs/prisma.json"//
-import pgJson from "../imgs//pg.json"//
-import pythonJson from "../imgs/python.json"
-import tsJson from "../imgs//ts.json"
-import gitJson from "../imgs/git.json"
-import solidityJson from "../imgs/solidity.json"
-import node from "../imgs/node.json"
-import downArrow from "../imgs/arrow-down.json"
 import { Rocket } from '../components/Rocket';
 import { Introduction } from '../components/Introduction';
 import { Skills } from '../components/Skills';
 import { Projects } from '../components/Projects';
-import ModalVideo from 'react-modal-video'
 import { PopupWidget } from "react-calendly";
 import { VideoModal } from '../components/VideoModal';
+import { Timeline } from '../components/Timeline';
+
+
 const skills = {
   "front-end": [
     "React",
@@ -59,6 +42,7 @@ function Home() {
   const [isOpen, setOpen] = useState(false);
   const [videoURL,setVideoURL] = useState('');
   const [theme,setTheme] = useState('Blue')
+
   const themes = {
     'Blue' : {
       tree: 'Tree-Blue.png', 
@@ -71,9 +55,20 @@ function Home() {
   }
 
   const changeTheme = (e)=>{
-    setTheme( (theme == "Red" ? "Blue" : "Red") )
-    
+    const newTheme =  (theme == "Red" ? "Blue" : "Red") 
+    setTheme(  newTheme )
+    localStorage.setItem("theme",  newTheme)
   }
+
+  useEffect(()=>{
+    const storedTheme = localStorage.getItem('theme')
+    
+    if ( storedTheme ){
+      setTheme( storedTheme)
+    }
+
+  },[])
+
   return (
     <div className="home">
       {
@@ -99,6 +94,10 @@ function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{marginTop:"-1%"}}>
           <path fill={themes[theme].color} d="M0,128L40,117.3C80,107,160,85,240,69.3C320,53,400,43,480,69.3C560,96,640,160,720,170.7C800,181,880,139,960,122.7C1040,107,1120,117,1200,138.7C1280,160,1360,192,1400,208L1440,224L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
         </svg>
+      </section>
+
+      <section className="container-start" >
+        <Timeline  color={themes[theme].color} />
       </section>
 
       <section className="container-start" >
