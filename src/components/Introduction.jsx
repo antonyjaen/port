4e84882462d071a1afaaf74a9e7eedf21b27dc64
antonyjaen <tonyjaenpop@gmail.com>
2024-05-21 02:29:16 -0600

    general changes

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -15,7 +15,7 @@ export const Introduction = () => {
     >
       <div className="h-screen flex flex-col justify-center items-center text-white text-xl mt-[-20vh]">
         <div className="text-center">
-          <h3 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl">Hello, I'm</h3>
+          <h3 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl">Hello, I&apos;m</h3>
         </div>
         <div className="mt-8 text-center">
           <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl">Antony Jaen Viquez</h1>
